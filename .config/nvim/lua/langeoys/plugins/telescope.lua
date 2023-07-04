89b65305df955488ecdbbb057ef2aa0f62de8338
oysandvik94 <oysandvik94@gmail.com>
2023-07-04 21:43:16 +0200

    Extract plugin spec

---
+++ .config/nvim/lua/langeoys/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+        vim.keymap.set('n', '<leader>fsd', builtin.lsp_document_symbols, {})
+        vim.keymap.set('n', '<leader>fsg', builtin.lsp_dynamic_workspace_symbols, {})
+        -- Find diagnostics filtered by type
+        vim.keymap.set('n', '<leader>fd', function()
+            builtin.diagnostics({ severity_limit = 'WARN' });
+        end)
+        vim.keymap.set('n', '<leader>ft', vim.cmd.Telescope, {})
+
+        vim.keymap.set('n', '<leader>lrr', builtin.lsp_references, {})
+
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        end)
+
+        require('telescope').load_extension('fzy_native')
+        require('telescope').load_extension('dap')
+        -- open file_browser with the path of the current buffer
+        vim.api.nvim_set_keymap(
+            "n",
+            "<space>fb",
+            ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+            { noremap = true }
+        )
+    end,
+}
