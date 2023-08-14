4feda5b5644897ef4d57efd0d5ce0c3a246677e1
oysandvik94 <oysandvik94@gmail.com>
2023-08-14 10:28:27 +0200

    Add compiler setting for omnisharp

--- .config/nvim/lua/langeoys/plugins/lsp/omnisharp.lua
+++ .config/nvim/lua/langeoys/plugins/lsp/omnisharp.lua
@@ -1,6 +1,15 @@
 local omnisharp = {}
 
+
 function omnisharp.Setup()
+    vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'cs' },
+        desc = 'Set dotnet compiler',
+        callback = function()
+            vim.cmd("compiler dotnet")
+        end,
+    })
+
     return {
         solution_first = true,
         handlers = {
