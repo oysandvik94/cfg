703e30e24446f3ab37fb248e69abd999120c6c87
oysandvik94 <oysandvik94@gmail.com>
2023-08-17 10:35:34 +0200

    Env vars

--- .local/bin/scripts/chtsh/tmux-cht.sh
+++ .local/bin/scripts/chtsh/tmux-cht.sh
@@ -8,9 +8,9 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.local/bin/scripts/chtsh/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less -R"
 elif [[ $selected == 'wiki' ]]; then
    tmux neww bash -c "less -i -p \"$query\" ~/wiki/vim.md" 
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
 fi
