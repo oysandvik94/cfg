7b42ca57a6c350e64afd4064bd13be54dcb0f584
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-09-19 09:00:19 +0200

    chore: clean up dumb fish stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 # Alias
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias fztmux='~/.local/bin/scripts/tmux-sessionizer'
+alias w3m='w3m -config ~/.config/w3m/config'
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -10,6 +11,7 @@ end
 set -x PATH $PATH $HOME/.dotnet/tools
 set -x DOTNET_ROOT $HOME/.dotnet
 set -x PATH $PATH $DOTNET_ROOT
+set -x W3M_DIR "~/.local/state/w3m"
 
 fish_add_path $HOME/.local/bin/scripts/
 
