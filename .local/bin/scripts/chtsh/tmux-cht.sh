43c389cb3fbd917b6a604fa4a9ed5b5ff1522872
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-10-11 21:57:15 +0200

    feat(tmux): add personal wiki to chtsht script

--- .local/bin/scripts/chtsh/tmux-cht.sh
+++ .local/bin/scripts/chtsh/tmux-cht.sh
@@ -4,13 +4,16 @@ if [[ -z $selected ]]; then
     exit 0
 fi
 
+if [[ $selected == 'wiki' ]]; then
+    tmux neww bash -c "zk edit --interactive"
+    exit 0
+fi
+
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.local/bin/scripts/chtsh/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "curl -s cht.sh/$selected/$query | less -R"
-elif [[ $selected == 'wiki' ]]; then
-   tmux neww bash -c "less -i -p \"$query\" ~/wiki/vim.md" 
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
 fi
