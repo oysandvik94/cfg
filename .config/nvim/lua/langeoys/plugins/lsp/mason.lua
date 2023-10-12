11d8e47f32153647ceb43078825d1b7c26ffa643
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-10-12 13:00:28 +0200

    feat(nvim): add new tsserver

--- .config/nvim/lua/langeoys/plugins/lsp/mason.lua
+++ .config/nvim/lua/langeoys/plugins/lsp/mason.lua
@@ -11,7 +11,6 @@ return {
 
         mason_lspconfig.setup({
             ensure_installed = {
-                "tsserver",
                 "eslint",
                 "omnisharp",
                 "lua_ls",
---
+++ .config/nvim/lua/langeoys/plugins/lsp/tsserver.lua
@@ -0,0 +1,28 @@
+return {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+    config = function()
+        require("typescript-tools").setup {
+            on_attach = function()
+                vim.keymap.set("n", "<leader>lo", [[
+                <cmd>TSToolsAddMissingImports<cr>
+                <cmd>TSToolsRemoveUnusedImports<cr>
+                <cmd>TSToolsOrganizeImports<cr>
+                ]],
+                    { desc = "Organize Imports" })
+                vim.keymap.set("n", "<leader>lO", "<cmd>TSToolsSortImports<cr>",
+                    { desc = "Sort Imports" })
+                vim.keymap.set("n", "<leader>lu", "<cmd>TSToolsRemoveUnused<cr>",
+                    { desc = "Removed Unused" })
+                vim.keymap.set("n", "<leader>lz", "<cmd>TSToolsGoToSourceDefinition<cr>",
+                    { desc = "Go To Source Definition" })
+                vim.keymap.set("n", "<leader>lR", "<cmd>TSToolsRemoveUnusedImports<cr>",
+                    { desc = "Removed Unused Imports" })
+                vim.keymap.set("n", "<leader>lF", "<cmd>TSToolsFixAll<cr>", { desc = "Fix All" })
+                vim.keymap.set("n", "<leader>lA", "<cmd>TSToolsAddMissingImports<cr>",
+                    { desc = "Add Missing Imports" })
+            end,
+        }
+    end
+}
