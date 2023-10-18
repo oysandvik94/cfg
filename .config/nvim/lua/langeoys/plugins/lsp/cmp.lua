efe90e6279d856e6272c3a0b9f8d72323dd835ed
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-10-18 08:55:04 +0200

    feat(nvim): add keybind for closing signature menu

--- .config/nvim/lua/langeoys/plugins/lsp/cmp.lua
+++ .config/nvim/lua/langeoys/plugins/lsp/cmp.lua
@@ -194,5 +194,8 @@ return {
         vim.keymap.set({ 'n' }, '<Leader>k', function()
             vim.lsp.buf.signature_help()
         end, { silent = true, noremap = true, desc = 'toggle signature' })
+        vim.keymap.set({ 'i' }, '<A-k>', function()
+            require('lsp_signature').toggle_float_win()
+        end, { silent = true, noremap = true, desc = 'toggle signature' })
     end,
 }
