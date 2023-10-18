1266eb10c0f49a6d4698840683aaf0b191081935
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-10-18 08:53:06 +0200

    feat(nvim): new plugins, aerial, alpha, autotag and sourcegragh

---
+++ .config/nvim/lua/langeoys/plugins/aerial.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {
+        filter_kind = false
+  },
+  -- Optional dependencies
+  dependencies = {
+     "nvim-treesitter/nvim-treesitter",
+     "nvim-tree/nvim-web-devicons"
+  },
+}
