c1cb15904180cf128c60377a79906e0c0f2e4441
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-11-01 08:33:31 +0100

    feat(rice): visual stuff

--- .local/bin/scripts/chtsh/tmux-cht.sh
+++ .local/bin/scripts/chtsh/tmux-cht.sh
@@ -13,7 +13,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.local/bin/scripts/chtsh/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | less -R"
+    tmux neww bash -c "curl -s https://cht.sh/$selected/$query | less -R"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
+    tmux neww bash -c "curl -s https://cht.sh/$selected~$query | less -R"
 fi
