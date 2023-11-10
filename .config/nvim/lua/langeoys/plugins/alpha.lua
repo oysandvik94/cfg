9365924ef61c4341ec571db6da227fd39b53ed4b
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2023-11-10 10:15:42 +0100

    feat(nvim): run alpha dashboard on dir open, but not file open

--- .config/nvim/lua/langeoys/plugins/alpha.lua
+++ .config/nvim/lua/langeoys/plugins/alpha.lua
@@ -1,57 +1,70 @@
 return {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function (_, dashboard)
-        require('alpha').setup(dashboard.opts)
-    end,
-    opts = function()
-		local dashboard = require('alpha.themes.dashboard')
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function(_, dashboard)
+		require("alpha").setup(dashboard.opts)
+
+		local start_alpha_group = vim.api.nvim_create_augroup("start_alpha", { clear = true })
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function(arg)
+				if string.sub(arg.file, -1) == "/" then
+					vim.cmd("Alpha")
+					return
+				end
+			end,
+			group = start_alpha_group,
+			pattern = "*",
+		})
+	end,
+	opts = function()
+		local dashboard = require("alpha.themes.dashboard")
 		local header = {
-	"       ,                                  ",
-	"       \\`-._           __                 ",
-	"        \\\\  `-..____,.'  `.               ",
-	"         :`.         /    \\`.             ",
-	"         :  )       :      : \\            ",
-	"          ;'        '   ;  |  :           ",
-	"          )..      .. .:.`.;  :           ",
-	"         /::...  .:::...   ` ;            ",
-	"         ; _ '    __        /:\\           ",
-	"         `:o>   /\\o_>      ;:. `.         ",
-	"        `-`.__ ;   __..--- /:.   \\        ",
-	"        === \\_/   ;=====_.':.     ;       ",
-	"         ,/'`--'...`--....        ;       ",
-	"              ;                    ;      ",
-	"            .'                      ;     ",
-	"          .'                        ;     ",
-	"        .'     ..     ,      .       ;    ",
-	"       :       ::..  /      ;::.     |    ",
-	"      /      `.;::.  |       ;:..    ;    ",
-	"     :         |:.   :       ;:.    ;     ",
-	"     :         ::     ;:..   |.    ;      ",
-	"      :       :;      :::....|     |      ",
-	"      /\\     ,/ \\      ;:::::;     ;      ",
-	"    .:. \\:..|    :     ; '.--|     ;      ",
-	"   ::.  :''  `-.,,;     ;'   ;     ;      ",
-	".-'. _.'\\      / `;      \\,__:      \\     ",
-	"`---'    `----'   ;      /    \\,.,,,/     ",
-	"                   `----`                 "		}
+			"       ,                                  ",
+			"       \\`-._           __                 ",
+			"        \\\\  `-..____,.'  `.               ",
+			"         :`.         /    \\`.             ",
+			"         :  )       :      : \\            ",
+			"          ;'        '   ;  |  :           ",
+			"          )..      .. .:.`.;  :           ",
+			"         /::...  .:::...   ` ;            ",
+			"         ; _ '    __        /:\\           ",
+			"         `:o>   /\\o_>      ;:. `.         ",
+			"        `-`.__ ;   __..--- /:.   \\        ",
+			"        === \\_/   ;=====_.':.     ;       ",
+			"         ,/'`--'...`--....        ;       ",
+			"              ;                    ;      ",
+			"            .'                      ;     ",
+			"          .'                        ;     ",
+			"        .'     ..     ,      .       ;    ",
+			"       :       ::..  /      ;::.     |    ",
+			"      /      `.;::.  |       ;:..    ;    ",
+			"     :         |:.   :       ;:.    ;     ",
+			"     :         ::     ;:..   |.    ;      ",
+			"      :       :;      :::....|     |      ",
+			"      /\\     ,/ \\      ;:::::;     ;      ",
+			"    .:. \\:..|    :     ; '.--|     ;      ",
+			"   ::.  :''  `-.,,;     ;'   ;     ;      ",
+			".-'. _.'\\      / `;      \\,__:      \\     ",
+			"`---'    `----'   ;      /    \\,.,,,/     ",
+			"                   `----`                 ",
+		}
 		dashboard.section.header.val = header
 		dashboard.section.buttons.val = {
-			dashboard.button('f', ' ' .. ' Find file', ':Telescope find_files <CR>'),
-			dashboard.button('n', ' ' .. ' New file', ':ene <BAR> startinsert <CR>'),
-			dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles <CR>'),
-			dashboard.button('g', ' ' .. ' Find text', ':Telescope live_grep <CR>'),
-			dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<CR>'),
-			dashboard.button('q', ' ' .. ' Quit', ':qa<CR>'),
+			dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+			dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+			dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+			dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 		}
 		for _, button in ipairs(dashboard.section.buttons.val) do
-			button.opts.hl = 'AlphaHeader'
-			button.opts.hl_shortcut = 'AlphaHeader'
+			button.opts.hl = "AlphaHeader"
+			button.opts.hl_shortcut = "AlphaHeader"
 		end
-        dashboard.section.terminal.width = 70
+		dashboard.section.terminal.width = 70
 		dashboard.section.terminal.height = 10
 		dashboard.section.terminal.opts.redraw = true
 		dashboard.section.terminal.opts.window_config.zindex = 1
 		return dashboard
 	end,
-};
+}
